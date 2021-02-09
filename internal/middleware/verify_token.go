a73c8ca65911fdb67e41c23fd5897ad0aa39c7f0
Justin <304602555@qq.com>
2021-02-09 15:12:02 +0800

    rpc调度改版

--- internal/middleware/verify_token.go
+++ internal/middleware/verify_token.go
@@ -20,7 +20,7 @@ func VerifyToken() gin.HandlerFunc {
 		uid := c.Query("uid")
 		uidInt64, err := strconv.ParseInt(uid, 10, 64)
 		if err != nil {
-			resp.RespCode(c, http.StatusUnauthorized, "未授权")
+			resp.RespCode(c, http.StatusUnauthorized, "未授权ll")
 			c.Abort()
 			return
 		}
